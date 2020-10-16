f6e52c9e6cc62b91f8c420c28bf6a6cc597462d2
Alastair Coote <alastair.coote@nytimes.com>
2020-10-15 21:32:28 -0400

    yet more macro noodling

--- esperanto-macros/src/lib.rs
+++ esperanto-macros/src/lib.rs
@@ -1,19 +1,89 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{self, DeriveInput};
+use std::error::Error;
+
+use proc_macro::{Span, TokenStream};
+use quote::{format_ident, quote, quote_spanned};
+use syn::{
+    self, parse::Parse, parse::ParseStream, parse_macro_input, spanned::Spanned, Attribute,
+    AttributeArgs, DeriveInput, FnArg, Ident, ImplItem, ImplItemMethod, Item, ItemFn, ItemImpl,
+    ItemStruct, Meta, MetaList, MetaNameValue,
+};
 
 #[proc_macro_attribute]
 pub fn js_export(attr: TokenStream, input: TokenStream) -> TokenStream {
-    let ast: DeriveInput = syn::parse(input).unwrap();
-    let name = ast.ident;
+    let mut parsed_input = syn::parse::<ItemImpl>(input).unwrap();
 
-    // println!("{}", ast.ident);
-    let gen = quote! {
-        impl #name {
-            fn new() -> Self {
+    if parsed_input.trait_ != None {
+        panic!("JSExport can only be used on a struct's own impl (for now)")
+    }
 
+    // the impl definition can include types and all kinds of things. We only
+    // care about methods, so let's get them:
+    let methods = parsed_input
+        .items
+        .iter()
+        .filter_map(|item| {
+            if let ImplItem::Method(method) = item {
+                return Some(method);
             }
-        }
-    };
-    gen.into()
+            return None;
+        })
+        .collect::<Vec<&ImplItemMethod>>();
+
+    // methods.first().unwrap().attrs
+    parsed_input.items.remove(0);
+
+    TokenStream::from(quote! {
+        #parsed_input
+    })
+
+    // let parsed_input = match syn::parse::<ItemImpl>(input) {
+    //     Ok(result) => result,
+    //     Err(err) => {
+    //         return syn::Error::from(err).to_compile_error().into();
+    //         // return syn::Error::new(Span::call_site().into(), "wat")
+    //         //     .to_compile_error()
+    //         //     .into();
+    //     }
+    // };
+
+    // // let target_impl = match parsed_input {
+    // //     // Item::Fn(f) => js_export_function(attr, f),
+    // //     // Item::Struct(s) => js_export_class(attr, s),
+    // //     Item::Impl(i) => i,
+    // //     _ => {
+    // //         return make_compile_error(
+    // //             parsed_input,
+    // //             "JSExport can only be used on an impl for now.",
+    // //         )
+    // //     }
+    // // };
+
+    // TokenStream::from(quote! {
+    //     #parsed_input
+    // })
+
+    // let ast: syn::ItemFn = syn::parse(input).unwrap();
+    // let name = ast.sig.ident.clone();
+
+    // // let name = ast.ident;
+    // // println!("{}", name);
+
+    // // // println!("{}", ast.ident);
+    // // let gen = quote! {
+    // //     #ast
+    // //     // impl Huh {
+    // //     //     fn new() -> Self {
+    // //     //         Huh {}
+    // //     //     }
+    // //     // }
+    // // };
+    // // gen.into()
+    // let varname = format_ident!("{}2", name);
+    // let gen = quote! {
+    //     #ast
+
+    //     fn #varname() {}
+    // };
+
+    // gen.into()
 }
--- esperanto-macros/tests/test.rs
+++ esperanto-macros/tests/test.rs
@@ -3,9 +3,26 @@ use esperanto_macros::js_export;
 struct Huh {}
 
 #[js_export]
-impl Huh {}
+impl Huh {
+    fn hmm() -> String {
+        "wow".to_string()
+    }
+}
+
+trait Watt {
+    fn two();
+}
+
+// #[js_export]
+impl Wat for Huh {}
+trait Wat {}
+
+trait Wat2<T> {
+    fn what();
+}
 
 #[test]
 fn it_works() {
-    Huh::new()
+    Huh::hmm();
+    // test22();
 }
